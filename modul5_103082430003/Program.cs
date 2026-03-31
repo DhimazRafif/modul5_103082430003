cb8d45e50de6baf5d2ecfe58a8ec44bdd7ec3da8
DhimazRafif <hanafidhimaz@gmail.com>
2026-03-31 18:29:20 +0700

    menambahkan class SimpleDataBase

--- modul5_103082430003/Program.cs
+++ modul5_103082430003/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class SimpleDataBase<T>
+{
+    private List <T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        this.storedData = new List <T>();
+        this.inputDates = new List <DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i=0;i<storedData.Count();i++)
+        {
+            Console.WriteLine($"Data : {i+1} berisi {storedData[i]}, yang disimpan pada waktu {inputDates[i]}");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<double> dataNIM = new SimpleDataBase<double>();
+        dataNIM.AddNewData(10.0);
+        dataNIM.AddNewData(30.0);
+        dataNIM.AddNewData(82.0);
+
+        dataNIM.PrintAllData();
+
+    }
+}
